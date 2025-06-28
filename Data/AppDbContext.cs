efeee76594dba7f7e27503251ed1727733da61de
John <johnpaulfrancis@gmail.com>
2025-06-29 01:38:42 +0530

    Added a DbContext class. Uses are: 1.  Acts as a Bridge Between Code and Database. 2. Tracks Changes to Entities 3. Maps C# Classes to Database Tables 4. Provides a Query API (LINQ) 5. Configuration and Customization 6. Integrates with Dependency Injection

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DotNetCoreMVC_CRUD.Models.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreMVC_CRUD.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<EmployeeMaster_Schema> EmployeeMaster { get; set; }
+    }
+}
