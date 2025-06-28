94f78d109f9a1bbddde7c4abb6438db9c9ec470e
John <johnpaulfrancis@gmail.com>
2025-06-29 01:42:47 +0530

    Register the DbContext with dependency injection

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using DotNetCoreMVC_CRUD.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Register the DbContext with dependency injection
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
